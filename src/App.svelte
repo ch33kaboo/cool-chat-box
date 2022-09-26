66ea2c85e18f8e250eac52fa90cd7a43703bc889
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-09-26 19:28:39 +0100

    tried await blocks, events, and event modifiers

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,31 @@
 <script>
-  import Nested from './lib/Nested.svelte'
-  const dodo = async () => {
-    let ha = await fetch('https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new')
-    console.log(ha);
+    import { invalid_attribute_name_character } from "svelte/internal";
+
+
+  const generateRandomNum = async () => {
+    let res = fetch('https://svelte.dev/tutorial/random-number')
+    res = await (await res).text()
+    return res
   }
-  dodo()
-  let name = 'somethingo'
-  let props = []
-  props = [...props, "miloudi"]
-  props = [...props, "metal"]
-  props = [...props,"69"]
-  let classs = 'btn btn-primary'
-  let string = '<h1>this is a string</h1>' 
-  let number = 0
-  const incNumber = () => {
-    number ++
+
+  let number = generateRandomNum()
+
+  let handleClick = (e) => {
+    number = generateRandomNum()
+    e.target.className += " cursor-not-allowed"
   }
-  let numbers = []
-  $: console.log(numbers);
-  $: mult = (number % 3) == 0
-  $: {
-    number
-    numbers = [...numbers, number]
-  }
-  $: double = number * 2
 </script>
 
-<main class="min-h-screen flex flex-col justify-around items-center" on:click="{incNumber}">
-  <div class="{classs}">number {number}</div>
-  <p>the double of that is: {double}</p>
-  {#if number % 2 == 0}
-  <Nested/>
-  {:else}
-  <Nested />
-  {/if}
-  {#if number % 3 == 0}
-    <button class="btn btn-secondary">mult 3</button>
-  {:else if number % 3 == 1}
-  <button class="btn btn-secondary">one added</button>
-  {:else}
-  <button class="btn btn-secondary">two added</button>
-  {/if}
-  {#each props as prop }
-    <h1>{prop}</h1>
-  {/each}
+<main class="h-screen flex flex-col justify-center items-center gap-4 text-center">
+  <button class="btn btn-secondary" on:click="{(e) => {handleClick(e)}}">click me</button>
+  {#await number}
+    <button class="btn loading">loading</button>
+  {:then data} 
+  <h1>the generated number is: {data}</h1>
+  {/await}
+
 </main>
 
 <style>
+
 </style>
